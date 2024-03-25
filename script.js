cce4365959dedb0693a3e1f8815175db3656ba5d
gega <133045741+gega22@users.noreply.github.com>
2024-03-25 21:00:14 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Function to open the pop-up
+function openPopup() {
+    document.getElementById("popup").style.display = "block";
+}
+
+// Function to close the pop-up
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+}
