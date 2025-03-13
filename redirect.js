c7674086fd0b180169a65031bbf0a1b88e462648
gega <133045741+gega22@users.noreply.github.com>
2025-03-14 00:00:32 +0400

    Add files via upload

---
+++ redirect.js
@@ -0,0 +1,3 @@
+  if (/Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+    window.location.href = "https://gega22.github.io/minecraft.net-mobile/"; // Change this to your mobile version URL
+  }
