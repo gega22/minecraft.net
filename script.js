4f9fe6437767e922459b010ce503d2c6d226d83b
gega <133045741+gega22@users.noreply.github.com>
2025-03-15 12:31:20 +0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Function to open the pop-up
-function openPopup() {
-    document.getElementById("popup").style.display = "block";
-}
-
-// Function to close the pop-up
-function closePopup() {
-    document.getElementById("popup").style.display = "none";
-}
