81e10a8224763305318ce90f9e22d2fafee49a6a
gega <133045741+gega22@users.noreply.github.com>
2025-06-27 16:31:43 +0400

    Delete autoplaysong.js

--- autoplaysong.js
+++
@@ -1,8 +0,0 @@
-        // Get the audio element
-        const audio = document.getElementById('audio');
-
-        // Add click event listener to the whole document
-        document.addEventListener('click', function() {
-            // Play the audio when any click happens
-            audio.play();
-        });
